6dbe707054492069770ed4e356fd82cc268678ce
some-dev <avolkov.dev@gmail.com>
2018-12-18 21:49:35 +0300

    write content of protocol.json file on startup

---
+++ src/Lykke.Service.NeoApi/Lifetime/StartupManager.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Common.Log;
+using Lykke.Common.Log;
+using Lykke.Sdk;
+
+namespace Lykke.Service.NeoApi.Lifetime
+{
+    public class StartupManager:IStartupManager
+    {
+        private readonly ILog _log;
+
+        public StartupManager(ILogFactory logFactory)
+        {
+            _log = logFactory.CreateLog(this);
+        }
+        public async Task StartAsync()
+        {
+            _log.Info("Content of protocol.json file", context: File.ReadAllTextAsync("protocol.json"));
+        }
+    }
+}
---
+++ src/Lykke.Service.NeoApi/Modules/ApiModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Lykke.Sdk;
+using Lykke.Service.NeoApi.Lifetime;
+
+namespace Lykke.Service.NeoApi.Modules
+{
+    public class ApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StartupManager>()
+                .As<IStartupManager>();
+        }
+    }
+}
